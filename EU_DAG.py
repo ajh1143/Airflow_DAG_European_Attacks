895187ca6b3204f6cd59781f5454ee80d97041ab
Andrew Holt <ajh.206@outlook.com>
2019-03-06 11:00:00 -0800

    Update EU_DAG.py
    Fixed spacing

--- EU_DAG.py
+++ EU_DAG.py
@@ -31,55 +31,86 @@ dag = DAG(
 def load_eu(ds, **kwargs):
     load()
     return 'File Loaded'
+  
+  
 def count_record(ds, **kwargs):
     count_records()
     return 'Counted records'
+  
+  
 def count_cols(ds, **kwargs):
     count_columns()
     return 'Extracted/Stored Columns'
+  
+  
 def list_country(ds, **kwargs):
     list_countries()
     return 'Extracted/Stored Countries'
+  
+  
 def Belg(ds, **kwargs):
     Belgium()
     return 'Extracted/Stored Belgium'
+  
+  
 def Den(ds, **kwargs):
     Denmark()
     return 'Extracted/Stored Denmark'
+  
+  
 def Fra(ds, **kwargs):
     France()
     return 'Extracted/Stored France'
+  
+  
 def Ger(ds, **kwargs):
     Germany()
     return 'Extracted/Stored Germany'
 def Gre(ds, **kwargs):
     Greece()
     return 'Extracted/Stored Greece'
+  
+  
 def Ire(ds, **kwargs):
     Ireland()
     return 'Extracted/Stored Ireland'
+  
+  
 def Ita(ds, **kwargs):
     Italy()
     return 'Extracted/Stored Italy'
+  
+  
 def Lux(ds, **kwargs):
     Luxembourg()
     return 'Extracted/Stored Luxembourg'
+  
+  
 def Neth(ds, **kwargs):
     Netherlands()
     return 'Extracted/Stored Netherlands'
+  
+  
 def Port(ds, **kwargs):
     Portugal()
     return 'Extracted/Stored Portugal'
+  
+  
 def Spa(ds, **kwargs):
     Spain()
     return 'Extracted/Stored Spain'
+  
+  
 def UK(ds, **kwargs):
     United_Kingdom()
     return 'Extracted/Stored UK'
+  
+  
 def map(ds, **kwargs):
     heatMap()
     return 'Heat Map Generated'
 
+  
 #---------------------------#
 #         Operators         #
 #---------------------------#
@@ -115,12 +146,16 @@ d = PythonOperator(
     python_callable=Belg,
     dag=dag,
 )
+
+
 e = PythonOperator(
     task_id='Denmark',
     provide_context=True,
     python_callable=Den,
     dag=dag,
 )
+
+
 f = PythonOperator(
     task_id='France',
     provide_context=True,
@@ -128,36 +163,47 @@ f = PythonOperator(
     dag=dag,
 )
 
+
 g = PythonOperator(
     task_id='Germany',
     provide_context=True,
     python_callable=Ger,
     dag=dag,
 )
+
+
 h = PythonOperator(
     task_id='Greece',
     provide_context=True,
     python_callable=Gre,
     dag=dag,
 )
+
+
 i = PythonOperator(
     task_id='Ireland',
     provide_context=True,
     python_callable=Ire,
     dag=dag,
 )
+
+
 j = PythonOperator(
     task_id='Italy',
     provide_context=True,
     python_callable=Ita,
     dag=dag,
 )
+
+
 k = PythonOperator(
     task_id='Luxembourg',
     provide_context=True,
     python_callable=Lux,
     dag=dag,
 )
+
+
 l = PythonOperator(
     task_id='Netherlands',
     provide_context=True,
@@ -165,18 +211,23 @@ l = PythonOperator(
     dag=dag,
 )
 
+
 m = PythonOperator(
     task_id='Portugal',
     provide_context=True,
     python_callable=Port,
     dag=dag,
 )
+
+
 n = PythonOperator(
     task_id='Spain',
     provide_context=True,
     python_callable=Spa,
     dag=dag,
 )
+
+
 o = PythonOperator(
     task_id='United_Kingdom',
     provide_context=True,
@@ -184,13 +235,13 @@ o = PythonOperator(
     dag=dag,
 )
 
+
 p = PythonOperator(
     task_id='Generate_Heat_Map',
     provide_context=True,
     python_callable=map,
     dag=dag,
 )
-
 
 
 #---------------------------#
